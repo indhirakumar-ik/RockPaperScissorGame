7fba2b80a8e97251e71f857b5caa01e968a615c7
Indhirakumar <indhirakumar49@gmail.com>
2026-08-18 22:58:22 +0530

    day 2 task

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log("Game ready guys let rock");
+let yourNumber=0
+let computerNumber=0;
+function playgame(playerChoice){
+    const a=Math.floor(Math.random()*3);
+    let computerChoice;
+    if(a===0){
+        computerChoice="rock";
+    }else if(a===1){
+        computerChoice="paper";
+    }else{
+        computerChoice="scissor";
+    }
+
+    document.getElementById("youroption").textContent="Your option : "+playerChoice;
+    document.getElementById("computeroption").textContent="computer option : "+computerChoice;
+
+    let result;
+    if(playerChoice===computerChoice){
+        result="tie";
+    }
+    else if(playerChoice==="rock" && computerChoice==="paper" || 
+        playerChoice==="paper" && computerChoice==="rock" ||
+        playerChoice=="scissor" && computerChoice==="paper"
+    ){
+        yourNumber++;
+        result="You win";
+    }else{
+        computerNumber++;
+        result="computer wins";
+    }
+
+
+    document.getElementById("result").textContent=result;
+
+    document.getElementById("yourscore").textContent="Your score : "+yourNumber;
+    document.getElementById("computerscore").textContent="computer score : "+computerNumber;
+}
